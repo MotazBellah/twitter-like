90209e3f424f0982284fe00cad1e5de04d6b6b3a
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-04 17:27:44 +0200

    Make sure the redirected url is safe

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a3lf!(wmp39h$&_&nj*hs!@x1(&(wqdkpnqii=+2tb!@v7*mqa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
 
 
 # Application definition
--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, JsonResponse, HttpResponseRedirect
+from django.utils.http import is_safe_url
+from django.conf import settings
 from .models import Tweet
 from .forms import TweetForm
+
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
 
 def home_view(request, *args, **kwargs):
     return render(request, 'tweets/home.html')
@@ -9,9 +15,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm()
 
     return render(request, 'components/forms.html', context={"form": form})
