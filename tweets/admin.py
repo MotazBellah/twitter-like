23667f6fcae54ee55ecb3732b28f4553ce2edbe6
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-05 19:41:50 +0200

    Added TweetLike to admine panel

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Tweet
+from .models import Tweet, TweetLike
 
 # Register your models here.
+
+
+class TweetLike(admin.TabularInline):
+    model = TweetLike
+
+
 # Search tweets by users
 class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLike]
     # display the user for each tweet
     list_display = ["__str__", "user"]
     search_fields = ["content", 'user__username', 'user__email']
