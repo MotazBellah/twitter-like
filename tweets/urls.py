d05a1cf8ab578b60e85254e7190967e12197cd38
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-05 17:53:32 +0200

    Delete tweet

--- tweets/urls.py
+++ tweets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('tweets', views.tweet_list_view),
     path('tweets/<int:tweet_id>', views.home_detail_view),
     path('create-tweet', views.tweet_create_view),
+    path('api/tweets/<int:tweet_id>/delete', views.home_delete_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -42,6 +42,22 @@ def home_detail_view(request, tweet_id):
     return Response(serializer.data, status=200)
 
 
+@api_view(['DELETE', "POST", "GET"])
+@permission_classes([IsAuthenticated])
+def home_delete_view(request, tweet_id):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+
+    qs = qs.filter(user=request.user)
+    if not qs.exists():
+        return Response({"message": "You cannot delete this tweet"}, status=401)
+
+    obj = qs.first()
+    obj.delete()
+    return Response({"message": "Tweet removed"}, status=200)
+
+
 @api_view(['GET'])
 def tweet_list_view(request):
     qs = Tweet.objects.all()
