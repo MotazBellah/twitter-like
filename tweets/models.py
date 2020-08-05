ff67b5b8eea504de06951fc5b2f50940f2d8c178
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-05 19:36:44 +0200

    Create table for like and use many to many field to define the relation between users, likes and tweets

--- tweets/models.py
+++ tweets/models.py
@@ -4,11 +4,20 @@ from django.conf import settings
 # Built-in user model
 User = settings.AUTH_USER_MODEL
 
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Tweet(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
 
     def __str__(self):
         return self.content
