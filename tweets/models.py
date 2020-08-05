ddec492a6a8df2d47d5d92396f559b55d713700e
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-05 14:20:50 +0200

    Added User using built-in django user, create superuser

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.conf import settings
+
+# Built-in user model
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     class Meta:
         # make the old tweets to be in the bottom
